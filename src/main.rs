3564f1163e42567635aaac81a644a0c53be09683
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-03-20 22:00:00 +0900

    initi_clocks_and_plls()を使わずに全て明示的に初期化

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{entry, hal::xosc::setup_xosc_blocking};
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
@@ -16,11 +16,29 @@ use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
+    clocks::{Clock, ClockSource, ClocksManager, InitError},
     pac,
+    pll::{
+        common_configs::{PLL_SYS_125MHZ, PLL_USB_48MHZ},
+        setup_pll_blocking,
+    },
     sio::Sio,
     watchdog::Watchdog,
 };
+
+use fugit::HertzU32;
+
+/// External high-speed crystal on the pico board is 12Mhz
+const EXTERNAL_XTAL_FREQ_HZ: HertzU32 = HertzU32::from_raw(12_000_000u32);
+
+/// RP2040の動作周波数 76.8MHz
+const RP2040_CLOCK_HZ: HertzU32 = HertzU32::from_raw(76_800_000u32);
+
+/// PIOの動作周波数 15.36MHz(48kHz*64*5) I2SのMCLKをPIOで作るので48kHzの整数倍にする
+const PIO_CLOCK_HZ: HertzU32 = HertzU32::from_raw(15_360_000u32);
+
+/// PIOの分周比率 15=RP2040動作周波数/PIO動作周波数
+const PIO_CLOCKDIV_INT: u32 = RP2040_CLOCK_HZ.raw() / PIO_CLOCK_HZ.raw();
 
 #[entry]
 fn main() -> ! {
@@ -30,19 +48,92 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
+    // Enable the xosc
+    let xosc = setup_xosc_blocking(pac.XOSC, EXTERNAL_XTAL_FREQ_HZ)
+        .map_err(InitError::XoscErr)
+        .ok()
+        .unwrap();
+
+    // Start tick in watchdog
+    watchdog.enable_tick_generation((EXTERNAL_XTAL_FREQ_HZ.raw() / 1_000_000) as u8);
+
+    let mut clocks = ClocksManager::new(pac.CLOCKS);
+
+    // Configure PLLs
+    //                   REF     FBDIV VCO            POSTDIV
+    // PLL SYS: 12 / 1 = 12MHz * 125 = 1500MHZ / 6 / 2 = 125MHz
+    // PLL USB: 12 / 1 = 12MHz * 40  = 480 MHz / 5 / 2 =  48MHz
+    let pll_sys = setup_pll_blocking(
         pac.PLL_SYS,
-        pac.PLL_USB,
+        xosc.operating_frequency().into(),
+        PLL_SYS_125MHZ,
+        &mut clocks,
         &mut pac.RESETS,
-        &mut watchdog,
     )
+    .map_err(InitError::PllError)
     .ok()
     .unwrap();
+
+    let pll_usb = setup_pll_blocking(
+        pac.PLL_USB,
+        xosc.operating_frequency().into(),
+        PLL_USB_48MHZ,
+        &mut clocks,
+        &mut pac.RESETS,
+    )
+    .map_err(InitError::PllError)
+    .ok()
+    .unwrap();
+
+    // Configure clocks
+    // CLK_REF = XOSC (12MHz) / 1 = 12MHz
+    clocks
+        .reference_clock
+        .configure_clock(&xosc, xosc.get_freq())
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
+
+    // CLK SYS = PLL SYS (125MHz) / 1 = 125MHz
+    clocks
+        .system_clock
+        .configure_clock(&pll_sys, pll_sys.get_freq())
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
+
+    // CLK USB = PLL USB (48MHz) / 1 = 48MHz
+    clocks
+        .usb_clock
+        .configure_clock(&pll_usb, pll_usb.get_freq())
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
+
+    // CLK ADC = PLL USB (48MHZ) / 1 = 48MHz
+    clocks
+        .adc_clock
+        .configure_clock(&pll_usb, pll_usb.get_freq())
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
+
+    // CLK RTC = PLL USB (48MHz) / 1024 = 46875Hz
+    clocks
+        .rtc_clock
+        .configure_clock(&pll_usb, HertzU32::from_raw(46875u32))
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
+
+    // CLK PERI = clk_sys. Used as reference clock for Peripherals. No dividers so just select and enable
+    // Normally choose clk_sys or clk_usb
+    clocks
+        .peripheral_clock
+        .configure_clock(&clocks.system_clock, clocks.system_clock.freq())
+        .map_err(InitError::ClockError)
+        .ok()
+        .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
