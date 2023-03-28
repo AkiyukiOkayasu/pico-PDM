84f68abe9b3b15107e92e00060bef653ee688009
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-03-28 14:53:22 +0900

    PLL設定にコメント追加とかallow dead_codeとか

--- src/rp2040_pll_settings_for_48khz_audio.rs
+++ src/rp2040_pll_settings_for_48khz_audio.rs
@@ -1,12 +1,15 @@
 //! RP2040_PLL_Settings for 48kHz audio
 //!
-//! 48kHzの整数倍かつ、jitterの少ない（low_vcoではない）設定をまとめたもの
+//! I2SのPIOを48kHzで動かすための設定を列挙したもの。
+//! I2SのPIOは15.36MHz (48kHz * 64 * 5)で動かすので、システムクロックはその整数倍にする。
 
 use fugit::HertzU32;
 use rp_pico::hal::pll::PLLConfig;
 
 /// RP2040を76.8MHzで動作させるためのPLL設定
+/// Cogはこの設定で動かしている（低消費電力目的）
 /// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
+#[allow(dead_code)]
 pub const SYS_PLL_CONFIG_76P8MHZ: PLLConfig = PLLConfig {
     vco_freq: HertzU32::MHz(1536),
     refdiv: 1,
@@ -14,17 +17,9 @@ pub const SYS_PLL_CONFIG_76P8MHZ: PLLConfig = PLLConfig {
     post_div2: 4,
 };
 
-/// RP2040を115.2MHzで動作させるためのPLL設定
-/// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
-pub const SYS_PLL_CONFIG_115P2MHZ: PLLConfig = PLLConfig {
-    vco_freq: HertzU32::MHz(1152),
-    refdiv: 1,
-    post_div1: 5,
-    post_div2: 2,
-};
-
 /// RP2040を153.6MHzで動作させるためのPLL設定
 /// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
+#[allow(dead_code)]
 pub const SYS_PLL_CONFIG_153P6MHZ: PLLConfig = PLLConfig {
     vco_freq: HertzU32::MHz(1536),
     refdiv: 1,
@@ -34,17 +29,9 @@ pub const SYS_PLL_CONFIG_153P6MHZ: PLLConfig = PLLConfig {
 
 /// RP2040を230.4MHzで動作させるためのPLL設定
 /// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
+#[allow(dead_code)]
 pub const SYS_PLL_CONFIG_230P4MHZ: PLLConfig = PLLConfig {
     vco_freq: HertzU32::MHz(1152),
-    refdiv: 1,
-    post_div1: 5,
-    post_div2: 1,
-};
-
-/// RP2040を288MHzで動作させるためのPLL設定
-/// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
-pub const SYS_PLL_CONFIG_288MHZ: PLLConfig = PLLConfig {
-    vco_freq: HertzU32::MHz(1440),
     refdiv: 1,
     post_div1: 5,
     post_div2: 1,
@@ -53,6 +40,7 @@ pub const SYS_PLL_CONFIG_288MHZ: PLLConfig = PLLConfig {
 /// RP2040を307.2MHzで動作させるためのPLL設定
 /// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
 /// クロックが速すぎてQSPI Flashの調整をしないといけないかもしれない
+#[allow(dead_code)]
 pub const SYS_PLL_CONFIG_307P2MHZ: PLLConfig = PLLConfig {
     vco_freq: HertzU32::MHz(1536),
     refdiv: 1,
@@ -63,6 +51,7 @@ pub const SYS_PLL_CONFIG_307P2MHZ: PLLConfig = PLLConfig {
 /// RP2040を384MHzで動作させるためのPLL設定
 /// $PICO_SDK/src/rp2_common/hardware_clocks/scripts/vcocalc.py
 /// クロックが速すぎてQSPI Flashの調整をしないといけないかもしれない
+#[allow(dead_code)]
 pub const SYS_PLL_CONFIG_384MHZ: PLLConfig = PLLConfig {
     vco_freq: HertzU32::MHz(1536),
     refdiv: 1,
