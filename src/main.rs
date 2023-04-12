e5ba1ad224a8edd75a8e32227c1d213c0ee1c7b6
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-04-12 15:37:44 +0900

    WIP 正に発散する問題の調査中

--- src/main.rs
+++ src/main.rs
@@ -299,18 +299,32 @@ fn main() -> ! {
             }
 
             for (i, e) in rx_buf.iter_mut().enumerate() {
-                let l_ones = l.count_ones() as i32; // PDMがpositiveのときの回数
-                let l_zeros = 16i32 - l_ones; // PDMがnegativeのときの回数
-                let l_ones = I1F31::from_bits(l_ones);
-                let l_zeros = I1F31::from_bits(l_zeros);
-                l_pdm += l_ones - l_zeros;
-                let r_ones = r.count_ones() as i32; // PDMがpositiveのときの回数
-                let r_zeros = 16i32 - r_ones; // PDMがnegativeのときの回数
-                let r_ones = I1F31::from_bits(r_ones);
-                let r_zeros = I1F31::from_bits(r_zeros);
-                r_pdm += r_ones - r_zeros;
                 let l = *e & 0b01010101_01010101_01010101_01010101; //Lchのみマスク
+                let l_ones = l.count_ones(); // PDMがpositiveのときの回数
+                let l_zeros = 16u32 - l_ones; // PDMがnegativeのときの回数
+                let l_diff: i32 = l_ones as i32 - l_zeros as i32;
+                let l_diff = I1F31::from_bits(l_diff);
+
+                if pdm_initialize_count_down == 0 {
+                    if i == 1 {
+                        if l_ones > l_zeros {
+                            info!("+");
+                        } else if l_ones < l_zeros {
+                            info!("-!!!!");
+                        } else {
+                            info!("0");
+                        }
+                    }
+                }
+
+                l_pdm += l_diff;
+
                 let r = *e & 0b10101010_10101010_10101010_10101010; //Rchのみマスク
+                let r_ones = r.count_ones(); // PDMがpositiveのときの回数
+                let r_zeros = 16u32 - r_ones; // PDMがnegativeのときの回数
+                let r_diff: i32 = r_ones as i32 - r_zeros as i32;
+                let r_diff = I1F31::from_bits(r_diff);
+                r_pdm += r_diff;
 
                 if i % (PDM_INTEGRAL_RATE / SAMPLE_RATE) as usize == 0 {
                     // 192kHz周期で積分するので4回に1回だけQueueに積むことで48kHzにダウンサンプル
