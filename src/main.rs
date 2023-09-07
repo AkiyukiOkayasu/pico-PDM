d919a402bdd7a8a7d7dd67459f5093ac5bd9e1e9
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-09-07 14:20:12 +0900

    2nd bootloaderを指定する

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 #![no_std]
 #![no_main]
 
+#[link_section = ".boot2"]
+#[no_mangle]
+#[used]
+pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_RAM_MEMCPY;
+
 use bsp::entry;
 use cic_fixed::CicDecimationFilter;
 use cortex_m::singleton;
