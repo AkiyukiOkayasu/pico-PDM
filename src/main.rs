636417da10aafaf01e9d593777bb8007522b2001
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-09-07 14:36:50 +0900

    ラズピコで使っているFLASH用の2nd bootloaderに戻す

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #[link_section = ".boot2"]
 #[no_mangle]
 #[used]
-pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_RAM_MEMCPY;
+pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 use bsp::entry;
 use cic_fixed::CicDecimationFilter;
